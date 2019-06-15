e0f8715fed176fc201af1e104d7752c185a83845
MAllen07 <mrsmallen07@gmail.com>
2019-06-15 11:49:38 -0400

    hit mvp

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,31 +1,16 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-const jwtKey =
-  process.env.JWT_SECRET || 'added a secret!';
+const jwtKey = process.env.JWT_SECRET || "added a secret!";
 
 // quickly see what this file exports
 module.exports = {
-  authenticate, generateToken
+  authenticate,
+  generateToken
 };
-                                                         
-function generateToken(user) {
-  const payload = {
-    subject: user.id,
-    username: user.username,
-  };
-
-  const options = {
-    expiresIn: '12h',
-  };
-  const secret = process.env.JWT_SECRET;
-  return jwt.sign(payload, secret, options)
-};
-
-
 
 // implementation details
 function authenticate(req, res, next) {
-  const token = req.get('Authorization');
+  const token = req.get("Authorization");
 
   if (token) {
     jwt.verify(token, jwtKey, (err, decoded) => {
@@ -37,7 +22,19 @@ function authenticate(req, res, next) {
     });
   } else {
     return res.status(401).json({
-      error: 'No token provided, must be set on the Authorization Header',
+      error: "No token provided, must be set on the Authorization Header"
     });
   }
+}
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1d"
+  };
+  const secret = jwtKey;
+  return jwt.sign(payload, secret, options);
 }
