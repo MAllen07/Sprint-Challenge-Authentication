03f53645452b59fad55aa078056336b0e19896aa
MAllen07 <mrsmallen07@gmail.com>
2019-06-15 11:39:50 -0400

    adjusted route to define user

--- config/routes.js
+++ config/routes.js
@@ -12,9 +12,8 @@ module.exports = server => {
 
 function register(req, res) {
   // implement user registration
-
-  const hash = bcrypt.hashSync(user.password, 14);
-  user.password = hash;
+  const hash = bcrypt.hashSync(req.body.password, 14);
+  req.body.password = hash;
 
   Users.add(req.body)
     .then(newUser => {
@@ -28,6 +27,28 @@ function register(req, res) {
 
 function login(req, res) {
   // implement user login
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+
+          token
+        });
+      } else {
+        res
+          .status(401)
+          .json({ message: "Wrong username or password. Try again." });
+      }
+    })
+    .catch(err => {
+      console.log("Login error", err);
+      res.status(500).json({ message: "Login error" });
+    });
 }
 
 function getJokes(req, res) {
